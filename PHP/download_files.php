5f7171ceeddc392d6ea24e34f6c0d3d9613a4b96
PeterNaggschga <per.natzschka@gmail.com>
2020-09-25 11:18:24 +0200

    exitmessages added

--- PHP/download_files.php
+++ PHP/download_files.php
@@ -11,22 +11,34 @@ foreach ($sportsClubXml->teams->team as $team){
 if (file_exists($rankingFileName)){
     $rankingsXml = simplexml_load_file($rankingFileName);
     if (strcmp($lastUpdate, $rankingsXml->matchSeries->resultsUpdated) == 0){
-        exit("Keine Änderungen in der Tabelle");
-    }else{
-        unlink($rankingFileName);
+        exit('Keine Änderungen in der Tabelle!');
     }
 }
 $matchSeriesXml = simplexml_load_file('https://ssvb.sams-server.de/xml/matchSeries.xhtml?apiKey=05b4a417-dba5-4382-b40d-ac2b6d6cb516');
-foreach ($matchSeriesXml->matchSeries as $item){
-    if (strcmp($item->shortName, $leagueShort ) == 0){
-        $seasonId = $item->id;
-        break;
+if (!($matchSeriesXml == false)){
+    foreach ($matchSeriesXml->matchSeries as $item){
+        if (strcmp($item->shortName, $leagueShort ) == 0){
+            $seasonId = $item->id;
+            break;
+        }
     }
+}else{
+    exit('Spielrundenübersicht konnte nicht geladen werden!');
 }
+
 if (isset($seasonId)){
     $url = 'https://ssvb.sams-server.de/xml/rankings.xhtml?apiKey=05b4a417-dba5-4382-b40d-ac2b6d6cb516&matchSeriesId=' . $seasonId;
     $rankingsXml = simplexml_load_file($url);
-    $rankingFile = fopen($rankingFileName, "w");
-    fwrite($rankingFile, $rankingsXml->asXML());
-    fclose($rankingFile);
+    if (!($rankingsXml == false)){
+        if (file_exists($rankingFileName)){
+            unlink($rankingFileName);
+        }
+        $rankingFile = fopen($rankingFileName, "w");
+        fwrite($rankingFile, $rankingsXml->asXML());
+        fclose($rankingFile);
+    }else{
+        exit('Tabelle konnte nicht geladen werden!');
+    }
+}else{
+    exit('Liga konnte nicht ermittelt werden!');
 }
